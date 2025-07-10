4fa1ac9e81023284f75054c07b65fc77207f7372
Sear Clímaco <sromero.netplus@gmail.com>
2025-07-10 11:11:02 -0600

    Facturación > Opciones > Catálogo de actividades

---
+++ src/pages/billing/options/activities/DataTable.vue
@@ -0,0 +1,131 @@
+<script setup>
+import { ref, watch, computed } from 'vue'
+import { useDataviewerStore } from 'stores/dataviewer/index.js'
+import { useClipboard } from 'src/utils/clipboard.js'
+import BaseDataTable from 'pages/baseComponents/BaseDataTable.vue'
+import BaseDialog from 'components/base/BaseDialog.vue'
+import DeleteItemDialog from 'components/base/DeleteItemDialog.vue'
+import ActivitiesDialog from 'components/billing/options/ActivitiesDialog.vue'
+
+const dataViewer = useDataviewerStore()
+const { copy } = useClipboard()
+const currentItem = ref(0)
+const showDeleteItem = ref(false)
+const deleteProps = ref([])
+const columns = [
+  { name: 'id', label: 'ID', sortable: true, align: 'center' },
+  {
+    name: 'status',
+    label: 'Estado',
+    filterable: true,
+    model: [],
+    filterURL: '/api/v1/general/estados',
+    options: [],
+    align: 'left',
+  },
+  { name: 'code', label: 'Código', align: 'left' },
+  { name: 'name', label: 'Nombre', align: 'left' },
+  { name: 'actions', label: '', align: 'center' },
+]
+const showForm = computed(() => dataViewer.get_dataViewer.showForm)
+const edit = (itm) => {
+  currentItem.value = itm
+  dataViewer.changeShowForm(2)
+}
+const showDeleteDialog = (id, name) => {
+  showDeleteItem.value = true
+  deleteProps.value = {
+    title: 'Eliminar Actividad',
+    message: `¿Deseas eliminar la actividad ${name} de los registros?`,
+    id: id,
+    url: '/api/v1/billing/options/activities/',
+  }
+}
+const resetShowDeleteItem = () => {
+  showDeleteItem.value = false
+}
+watch(showForm, (newVal) => {
+  if (newVal === 1) {
+    currentItem.value = 0
+  } else {
+    dataViewer.fetch({ force: true })
+  }
+})
+</script>
+<template>
+  <div>
+    <template v-if="showDeleteItem">
+      <DeleteItemDialog
+        :data="deleteProps"
+        :visible="showDeleteItem"
+        @hide-dialog="resetShowDeleteItem"
+      />
+    </template>
+
+    <template v-if="showForm !== 0">
+      <BaseDialog :id="currentItem" :content="ActivitiesDialog" />
+    </template>
+
+    <BaseDataTable :columns="columns">
+      <template v-slot:body="{ props }">
+        <q-tr :props="props">
+          <!--    ID    -->
+          <q-td key="id" class="text-left copy-text" :props="props" @click="copy(props.row?.id)">
+            {{ props.row?.id }}
+          </q-td>
+
+          <!--    Status    -->
+          <q-td key="status" :props="props">
+            <q-badge
+              :color="props.row?.status?.id ? 'primary' : 'red-10'"
+              :label="props.row?.status?.name"
+              class="text-center text-weight-bold q-py-xs"
+            />
+          </q-td>
+
+          <!--    Code     -->
+          <q-td
+            key="code"
+            class="text-left copy-text"
+            :props="props"
+            @click="copy(props.row?.code)"
+          >
+            {{ props.row?.code }}
+          </q-td>
+
+          <!--    Name    -->
+          <q-td
+            key="name"
+            class="text-left copy-text"
+            :props="props"
+            @click="copy(props.row?.name)"
+          >
+            {{ props.row?.name }}
+          </q-td>
+
+          <!--    Actions    -->
+          <q-td key="actions" :props="props">
+            <q-btn-group>
+              <q-btn color="primary" icon="edit" size="sm" @click="edit(props.row?.id)">
+                <q-tooltip transition-show="fade" transition-hide="flip-left" class="bg-grey-10">
+                  Editar info. de {{ props.row?.name }}
+                </q-tooltip>
+              </q-btn>
+              <q-btn
+                color="negative"
+                icon="delete_forever"
+                size="sm"
+                @click="showDeleteDialog(props.row?.id, props.row?.name)"
+              >
+                <q-tooltip transition-show="fade" transition-hide="flip-left" class="bg-grey-10">
+                  Eliminar a {{ props.row?.name }}
+                </q-tooltip>
+              </q-btn>
+            </q-btn-group>
+          </q-td>
+        </q-tr>
+      </template>
+    </BaseDataTable>
+  </div>
+</template>
+<style lang="sass" scoped></style>
--- src/router/billing.js
+++ src/router/billing.js
@@ -5,10 +5,16 @@ const billing = [
     meta: { requiresAuth: true },
     children: [
       {
-        path: 'opciones/tipos-documentos',
-        name: 'facturacion.opciones.tipos',
+        path: 'opciones/tipos/documentos',
+        name: 'facturacion.opciones.tipos.documentos',
         component: () => import('pages/billing/options/documents/IndexPage.vue'),
         meta: { requiresAuth: true, title: 'Tipos de documentos' },
+      },
+      {
+        path: 'opciones/tipos/actividades',
+        name: 'facturacion.opciones.tipos.actividades',
+        component: () => import('pages/billing/options/activities/IndexPage.vue'),
+        meta: { requiresAuth: true, title: 'Tipos de actividades' },
       },
     ],
   },
