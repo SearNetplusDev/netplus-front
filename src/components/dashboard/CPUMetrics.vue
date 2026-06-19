dabce809f7f2bad92e34390ac75a45114c64cbf2
Sear Clímaco <sromero.netplus@gmail.com>
2026-06-19 17:31:23 -0600

    Gráfico con las métricas del estado del auth server.

---
+++ src/components/dashboard/CPUMetrics.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { reactive, onMounted, ref, defineAsyncComponent } from 'vue'
+import { api } from 'src/utils/api.js'
+import { useLoading } from 'src/utils/loader.js'
+import { useNotifications } from 'src/utils/notification.js'
+
+const ApexChart = defineAsyncComponent(() => import('vue3-apexcharts'))
+const { showLoading, hideLoading } = useLoading()
+const { showNotification } = useNotifications()
+const chartOptions = reactive({
+  chart: {
+    type: 'radialBar',
+    width: 200,
+    height: 300,
+    background: 'transparent',
+    foreColor: '#e2e8f0',
+  },
+  labels: ['CPU', 'Memoria', 'Almacenamiento'],
+  colors: [
+    '#00B4D8',
+    '#48CAE4',
+    '#90E0EF',
+  ],
+  title: {
+    text: '',
+    align: 'center',
+    style: {
+      fontSize: '16px',
+      fontWeight: 'bold',
+      color: '#ffffff',
+    },
+  },
+  plotOptions: {
+    radialBar: {
+      track: {
+        background: '#334155',
+      },
+      dataLabels: {
+        name: { fontSize: '14px' },
+        value: { fontSize: '16px', color: '#ffffff' },
+        total: {
+          show: true,
+          label: 'Promedio',
+          color: '#ffffff',
+          formatter: (val) => {
+            const total = val.globals.seriesTotals.reduce((a, b) => a + b, 0)
+            return (total / val.globals.seriesTotals.length).toFixed(2) + '%'
+          },
+        },
+      },
+    },
+  },
+  legend: {
+    show: true,
+    position: 'bottom',
+    labels: {
+      colors: '#ffffff',
+    },
+  },
+})
+const chartSeries = ref([])
+const colorByThreshold = (pct) => {
+  if (pct > 90) return '#ff4560'
+  if (pct > 75) return '#feb019'
+  return ['#00e396']
+}
+const getData = async () => {
+  showLoading()
+  try {
+    const { data } = await api.get('/api/v1/dashboard/resources')
+    if (data?.data) {
+      const { cpu, memory, storage, system } = data.data
+      chartSeries.value = [
+        cpu.load_pct,
+        Number(memory.used_pct.toFixed(2)),
+        Number(storage.used_pct.toFixed(2)),
+      ]
+      const dynamicColors = [
+        colorByThreshold(cpu.load_pct),
+        colorByThreshold(memory.used_pct),
+        colorByThreshold(storage.used_pct),
+      ]
+
+      chartOptions.colors = [...dynamicColors]
+      chartOptions.title.text = system.board_name
+
+      console.log('Series: ', chartSeries.value)
+      console.log(`Colores: ${dynamicColors}`)
+      console.log(`Colores Opcion: ${chartOptions.colors}`)
+    } else {
+      showNotification('Error', 'Algo ha salido mal.', 'red-10')
+    }
+  } catch (err) {
+    showNotification(
+      'Error',
+      err.response?.data?.message ?? err.message ?? 'Error inesperado',
+      'red-10',
+    )
+  } finally {
+    setTimeout(() => {
+      hideLoading()
+    }, 150)
+  }
+}
+onMounted(async () => {
+  await getData()
+})
+</script>
+
+<template>
+  <q-card flat class="custom-cards">
+    <apex-chart
+      type="radialBar"
+      :options="chartOptions"
+      :series="chartSeries"
+      :key="JSON.stringify(chartOptions.colors)"
+    />
+  </q-card>
+</template>
+
+<style lang="sass" scoped>
+.custom-header
+  color: #f8fafc
+  font-weight: 600
+  font-size: 18px
+</style>
--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useAuthStore } from 'stores/auth.js'
 import ClientTypes from 'components/dashboard/ClientTypes.vue'
+import CPUMetrics from 'components/dashboard/CPUMetrics.vue'
 
 const auth = useAuthStore()
 </script>
@@ -13,10 +14,16 @@ const auth = useAuthStore()
       </q-card-section>
 
       <div class="row full-width q-pa-md">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 q-ma-sm">
           <ClientTypes />
         </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 q-ma-sm">
+          <c-p-u-metrics />
+        </div>
       </div>
+
+      <div class="row full-width q-pa-md"></div>
     </q-card>
   </q-page>
 </template>
